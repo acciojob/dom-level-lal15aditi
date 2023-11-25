595aa253a270c93cbd3bb42e74db49b77a51a353
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2023-11-25 17:53:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+  const targetElement = document.getElementById('target');
+  let domLevel = 0;
+  
+  // Traverse through the parent elements until reaching the root (html) element
+  let parent = targetElement.parentElement;
+  while (parent) {
+    domLevel++;
+    parent = parent.parentElement;
+  }
+
+  // Display the DOM level using alert
+  alert(`The level of the element is: ${domLevel}`);
+});
